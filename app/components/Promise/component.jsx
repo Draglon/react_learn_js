f0d66134ef3d1ebe76cfe7058e807f7df449f035
serg <s.nazarchuck@gmail.com>
2022-02-14 10:12:10 +0200

    add examples

--- app/components/Promise/component.jsx
+++ app/components/Promise/component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Typography, Layout } from 'antd';
 
-const PromisesComponent = ({ onPlay }) => (
+const PromisesComponent = ({
+  onPlay,
+  onPlayAnimation,
+  onPlayQuery,
+  onPlayPromisify,
+}) => (
   <Layout className="layout">
     <div className="top-margin">
       <Typography.Title>Промисы</Typography.Title>
     </div>
 
-    <div>
-      <h2>Привер промиса</h2>
+    <div className='example'>
+      <h1 className='example__title'>Пример 1:</h1>
+      <h2 className='example__subtitle'>Пример с задержкой времени:</h2>
       <p className='text'>Функция выполнится автоматически, при вызове new Promise, через 1 секунду сигнализировать, что задача выполнена с результатом "done"</p>
       <div className='html-code'>
         <pre>
@@ -18,6 +24,27 @@ const PromisesComponent = ({ onPlay }) => (
         </pre>
         <button type="button" className='btn-play' onClick={onPlay}>Play</button>
       </div>
+    </div>
+
+    <div className='example'>
+      <h1 className='example__title'>Пример 2:</h1>
+      <h2 className='example__subtitle'>Пример анимация круга с помощью промиса:</h2>
+      <button type="button" className='btn-play' onClick={onPlayAnimation}>Play</button>
+      <div className='circle-wrapper'>
+        <div id="circle" className='circle'></div>
+      </div>
+    </div>
+
+    <div className='example'>
+      <h1 className='example__title'>Пример 3:</h1>
+      <h2 className='example__subtitle'>Цепочка промисов:</h2>
+      <button type="button" className='btn-play' onClick={onPlayQuery}>Play</button>
+    </div>
+    
+    <div className='example'>
+      <h1 className='example__title'>Пример 4:</h1>
+      <h2 className='example__subtitle'>Промисификация:</h2>
+      <button type="button" className='btn-play' onClick={onPlayPromisify}>Play</button>
     </div>
   </Layout>
 );
--- app/components/Promise/container.jsx
+++ app/components/Promise/container.jsx
@@ -15,11 +15,69 @@ class PromisesContainer extends Component {
     );
   };
 
+  showCircle = (cx, cy, radius) => {
+    let div = document.getElementById('circle');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+
+        div.addEventListener('transitionend', function handler() {
+          div.removeEventListener('transitionend', handler);
+          resolve(div);
+        });
+      }, 0);
+    })
+  }
+
+  handlePlayAnimation = () => {
+    this.showCircle(150, 150, 100).then(div => {
+      div.classList.add('message-ball');
+      div.append("Hello, world!");
+    });
+  }
+
+  handlePlayQuery = () => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => resolve(1), 1000);
+    }).then((result) => {
+      alert(`результат умножения на 10: ${result}`);
+      return result * 10;
+    }).then((result) => {
+      alert(`результат умножения на 10: ${result}`);
+      return result * 10;
+    }).then((result) => {
+      alert(`результат умножения на 10: ${result}`);
+      return result;
+    });
+  }
+
+  loadScriptPromise = (src) => {
+    return new Promise((resolve, reject) => {
+      loadScript(src, (err, script) => {
+        if (err) reject(err)
+        else resolve(script);
+      });
+    })
+  }
+
+  handlePlayPromisify = () => {
+    this.loadScriptPromise('path/script.js');
+  }
+
   render() {
     return (
       <PromisesComponent
         {...this.props}
         onPlay={this.handlePlay}
+        onPlayAnimation={this.handlePlayAnimation}
+        onPlayQuery={this.handlePlayQuery}
+        onPlayPromisify={this.handlePlayPromisify}
       />
     );
   }
